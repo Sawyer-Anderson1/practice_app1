c033bdf93239c0c0c5dc41529c476ef6fb6c9134
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2025-01-20 19:52:10 -0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() => runApp(MaterialApp(
   home: NinjaCard(),
 ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
   const NinjaCard({super.key});
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +31,11 @@ class NinjaCard extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(10.0),
-        child: Center(
-          child: Column(
-          children: [
-            Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
               margin: EdgeInsets.fromLTRB(10.0, 75.0, 10.0, 75.0),
               decoration: BoxDecoration(
                 border: Border.all(
@@ -35,52 +43,111 @@ class NinjaCard extends StatelessWidget {
                   width: 10.0
                 ),
               ),
-              child: Image(
-                image: AssetImage("assets/ninja.jpg"),
-              ),
-            ),
-            Divider(
-              color: Colors.white30,
-              thickness: 1.0,
-            ),
-            Text(
-              "NAME: Sawyer Anderson",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2,
-                fontSize: 30,
-              ),
-            ),
-            Text(
-              "ID: 2345-23",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2,
-                fontSize: 25
-              ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+
                 children: [
-                  Icon(
-                    Icons.mail,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    "sawyerlynnanderson@icloud.com",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
-                    ),
+                  Container(
+                    child: Image(
+                      alignment: Alignment.center,
+                      image: AssetImage("assets/ninja.jpg"),
+                                  ),
                   ),
                 ],
               ),
             ),
-            SizedBox(
-              height: 35.0,
-            )
-            ],
+          ),
+          Divider(
+            color: Colors.white30,
+            thickness: 1.0,
+          ),
+          Text(
+            "NAME:",
+            style: TextStyle(
+              color: Colors.white30,
+              letterSpacing: 2,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "Sawyer Anderson",
+            style: TextStyle(
+              color: Colors.redAccent,
+              letterSpacing: 2,
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            "ID:",
+            style: TextStyle(
+              color: Colors.white30,
+              letterSpacing: 2,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "2345-23",
+            style: TextStyle(
+              color: Colors.redAccent,
+              letterSpacing: 2,
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            "Ninja Level:",
+            style: TextStyle(
+              color: Colors.white30,
+              letterSpacing: 2,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "$ninjaLevel",
+            style: TextStyle(
+              color: Colors.redAccent,
+              letterSpacing: 2,
+              fontSize: 30,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Icon(
+                  Icons.mail,
+                  color: Colors.white,
+                ),
+                Text(
+                  "sawyerlynnanderson@icloud.com",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 35.0,
+          )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel++;
+          });
+        },
+        backgroundColor: Colors.redAccent,
+        elevation: 100,
+        child: Center(
+          child: Text(
+            textAlign: TextAlign.center,
+          "Level Up!",
+          style: TextStyle(
+              color: Colors.white
+          ),
           ),
         ),
       ),
